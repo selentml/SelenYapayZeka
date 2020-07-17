073c441081dce6225a2104f7da8739bfc1aef043
Apelios <funnycode4@gmail.com>
2020-07-18 00:10:02 +0300

    slm nbrff

---
+++ Controllers/UrunController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SelenYapayZekaGit.Classes.String;
+
+namespace SelenYapayZekaGit.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET
+        public IActionResult Ekle()
+        {
+            ViewBag.Title = "Ürün Ekle".AsTitle();
+            return View();
+        }
+    }
+}
