74f32dc9fe6ac9d4e387560b5a8e138f587c9aa1
brk-Desktop <funnycode4@gmail.com>
2020-07-17 08:15:24 +0300

    CreateDb

---
+++ DAL/Classes/Kategori.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SelenYapayZekaGit.DAL.Classes
+{
+    public class Kategori
+    {
+        [Key] public int Id { get; set; }
+        public string Isim { get; set; }
+        
+    }
+}
---
+++ DAL/SelenDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SelenYapayZekaGit.DAL.Classes;
+
+namespace SelenYapayZekaGit.DAL
+{
+    public class SelenDB : DbContext
+    {
+        public DbSet<Kategori> Kategori { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(
+                @"Server=tcp:selenyapayzekadb.database.windows.net,1433;Initial Catalog=selenDb;Persist Security Info=False;User ID=selenyapayzeka;Password=aSd415263.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
