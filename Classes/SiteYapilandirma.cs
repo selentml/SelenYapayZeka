05e029568c00a3ff1b26a385c1d0ee414bd006b3
selen.yel <selen.yel@blue-chip.com.tr>
2020-07-18 08:50:18 +0300

    selenim ben ülen

--- Classes/SiteYapilandirma.cs
+++ Classes/SiteYapilandirma.cs
@@ -8,13 +8,13 @@
 
         public static string mailAdres { get; set; } = "mail@selenyapayzekaazurewebsites.com";
 
-        public static string motto { get; set; } = "motto buraya gelecek";
+        public static string motto { get; set; } = "oyuncak bizim işimiz hüloğğğ";
 
         public static string faceUrl { get; set; } = "#";
         public static string twitUrl { get; set; } = "#";
         public static string instaUrl { get; set; } = "#";
         public static string pinterestUrl { get; set; } = "#";
-        public static string adres { get; set; } = "#";
+        public static string adres { get; set; } = "bursel mah bursel sok no : bursel";
         public static string telefon { get; set; } = "#";
     }
 }
