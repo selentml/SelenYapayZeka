69d4380da50d6269f8ddb54800b2fed6b437c38e
Apelios <funnycode4@gmail.com>
2020-07-18 10:08:42 +0300

    bebek ekleme

---
+++ Classes/JsonReturner.cs
@@ -0,0 +1,24 @@
+﻿namespace SelenYapayZekaGit.Classes
+{
+    public class JsonReturner
+    {
+        public bool _durum;
+        public string _mesaj;
+
+        public JsonReturner(bool durum,string mesaj)
+        {
+            _durum = durum;
+            _mesaj = mesaj;
+        }
+
+        public static JsonReturner Basarili(string mesaj)
+        {
+            return new JsonReturner(true, mesaj);
+        }
+
+        public static JsonReturner Basarisiz(string mesaj)
+        {
+            return new JsonReturner(false,mesaj);
+        }
+    }
+}
---
+++ Controllers/SelenApi.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SelenYapayZekaGit.Classes;
+using SelenYapayZekaGit.DAL;
+using SelenYapayZekaGit.DAL.Classes;
+
+namespace SelenYapayZekaGit.Controllers
+{
+    public class SelenApi : Controller
+    {
+
+        private SelenDB _db;
+
+        public SelenApi(SelenDB db)
+        {
+            _db = db;
+        }
+        
+        [HttpPost]
+      
+        public JsonResult UrunEkle(string urunIsim, string resimUrl, int kategori, string etiket)
+        {
+        
+            if (string.IsNullOrEmpty(urunIsim))
+            {
+                return Json(new {_durum = false,_mesaj = "Ürün ismi boş bırakılamaz."});
+            }
+
+            if (string.IsNullOrEmpty(resimUrl))
+            {
+                return Json(new {_durum = false,_mesaj = "Resim url boş bırakılamaz."});
+            }
+
+            if (kategori == -1)
+            {
+                return Json(new {_durum = false,_mesaj = "Kategori seçmediniz."});
+            }
+
+            etiket = etiket.ToLower();
+            var etiketList = etiket.Split(',');
+            List<Etiket> etiketler = new List<Etiket>();
+            var urun = new Urun();
+            urun.Isim = urunIsim;
+            urun.ResimUrl = resimUrl;
+            urun.Kategori = _db.Kategori.First(a => a.KategoriId == kategori);
+            _db.Urun.Add(urun);
+            _db.SaveChanges();
+            foreach (var et in etiketList)
+            {
+                var eklenecekEtiket = new Etiket(){Isim = et};
+                if (_db.Etkiket.Any(a => a.Isim == eklenecekEtiket.Isim))
+                {
+                    eklenecekEtiket = _db.Etkiket.First(a => a.Isim == eklenecekEtiket.Isim);
+                }
+                else
+                {
+                    _db.Etkiket.Add(eklenecekEtiket);
+                }
+                _db.UrunEtiket.Add(new UrunEtkiket() {Urun = urun, Etiket = eklenecekEtiket});
+
+            }
+
+            _db.SaveChanges();
+            return Json(new {_durum = true,_mesaj = "Ürün ekleme başalarılı."});
+
+        }
+    }
+}
